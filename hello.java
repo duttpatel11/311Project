66038868caca7d1e187baaee71b499d8bd4fa4ad
dutt patel <djp5735@psu.edu>
2020-04-02 14:15:34 -0400

    iss53 finally fixed

--- hello.java
+++ hello.java
@@ -3,11 +3,8 @@ public class hello {
 	public void doSoemthingIntering() {
 		int a = 10;
 		int b = 20;
-<<<<<<< HEAD
-		a = a+ b;
-=======
 		a = a* b;
->>>>>>> iss53
+
 	}
 	public static void main{String[] args} {
 	System.out.println("Hello Git");
