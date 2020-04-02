8f70cda599544c32b0256a5a1d701e105b3cb2e2
dutt patel <djp5735@psu.edu>
2020-04-02 14:06:50 -0400

    fixing issue 53

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 public class hello {
 	
 	public void doSoemthingIntering() {
-		//doing interesting stuff here
+		int a = 10;
+		int b = 20;
+		a = a * b;
 	}
 	public static void main{String[] args} {
 	System.out.println("Hello Git");
